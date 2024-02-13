bddcb1a25f4860aef547ee7a5ede44f4f8105b83
kauryzende <159888884+kauryzende@users.noreply.github.com>
2024-02-13 10:18:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+ const alternatives = [
+    {text:"", images:"images/cat-01.gif"},
+    {text:"mouahaha tu n'as pas le choix", images:"images/cat-02.gif"},
+    {text:"Shpouck shpouck", images:"images/cat-03.gif"},
+    {text:"miam miam ramen?", images:"images/cat-04.gif"},
+    {text:"snif snif", images:"images/cat-05.gif"}
+];
+const ohyes = {text:"hehehe you're in danger", images:"images/cat-yes.gif"};
+const title = document.querySelector('.title');
+const text = document.querySelector('.text');
+const cat = document.querySelector('.cat');
+const buttons = document.querySelectorAll('.button');
+const errorButton = document.querySelector('.button__error');
+
+let count = 0;
+function updateDisplay(item) {
+    console.log(item);
+    cat.src = item.images;
+    text.innerHTML = item.text;
+}
+
+errorButton.addEventListener('click', () => {
+    count = 0;
+    updateDisplay(alternatives[count]);
+    buttons.forEach(btn => btn.style.display = 'inline-block');
+    errorButton.style.display = 'none';
+});
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if(button.textContent === 'SÍ'){
+            updateDisplay(ohyes);
+            buttons.forEach(btn => btn.style.display = 'none');
+        }
+        if (button.textContent === 'NO'){
+            count++;
+            if(count < alternatives.length){
+                updateDisplay(alternatives[count]);
+            } else {
+                buttons.forEach(btn => btn.style.display = 'none');
+                errorButton.style.display = 'inline-block';
+            }
+        }
+    });
+});
